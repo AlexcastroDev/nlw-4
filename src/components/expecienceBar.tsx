e2be18b360c83397dc74155310bb082833638d3b
Alexandro castro <alexandro.castro@conexasaude.com.br>
2021-02-23 08:42:56 -0300

    commit aula 01

---
+++ src/components/expecienceBar.tsx
@@ -0,0 +1,12 @@
+export function ExperienceBar() {
+    return (
+        <header className="experience-bar">
+            <span>0 xp</span>
+            <div>
+                <div style={{width: '50%'}}/>
+                <span style={{left: '50%'}} className="current-experience">300exp</span>
+            </div>
+            <span>688 xp</span>
+        </header>
+    )
+}
